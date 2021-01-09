079447e07f4bf16bad409c15d54d7dae1b5efd06
Chong Kok Sian <ckh_5316@yahoo.com>
2021-01-09 10:35:05 +0800

    change title, background and image

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 class App extends Component {
@@ -32,10 +33,12 @@ class App extends Component {
 		<h1>Loading</h1> :
 		(
 			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
+				<h1 className='f1 pa4'>Robo Invaders</h1>
 				<SearchBox searchChange={this.onSearchChange} />
 				<Scroll>
-					<CardList robots={filteredRobots} />
+					<ErrorBoundary>
+						<CardList robots={filteredRobots} />
+					</ErrorBoundary>
 				</Scroll>
 			</div>
 		);
